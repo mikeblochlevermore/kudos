1725ece86306950abe5ea1ccbed84fbb612d262f
Mike Bloch-Levermore <interactivephilosophy@gmail.com>
2023-11-16 16:31:02 +0100

    settings.py updated

--- project4/settings.py
+++ project4/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Define the locations of your static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'network/static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
