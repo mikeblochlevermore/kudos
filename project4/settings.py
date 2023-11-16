cef87ff83f44a93af29edefb02ad7bcb1823b7f3
Mike Bloch-Levermore <interactivephilosophy@gmail.com>
2023-11-16 16:40:19 +0100

    STATIC_URL = 'static/'

--- project4/settings.py
+++ project4/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Define the locations of your static files
 STATICFILES_DIRS = [
